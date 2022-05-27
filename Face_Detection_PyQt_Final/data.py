efc8da669489e86e84c09a84fdaa46bf72a62bd7
divyangana009 <87435764+divyangana009@users.noreply.github.com>
2022-05-27 15:32:44 +0530

    Add files via upload

---
+++ Face_Detection_PyQt_Final/data.py
@@ -0,0 +1,66 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QTableWidget, QTableWidgetItem, QPushButton, QHeaderView, QHBoxLayout, QVBoxLayout
+from PyQt5.QtCore import Qt
+import pandas as pd # pip install pandas
+import csv
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.window_width, self.window_height = 700, 500
+        self.resize(self.window_width, self.window_height)
+        self.setWindowTitle('Attendance Records')
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        self.table = QTableWidget()
+        layout.addWidget(self.table)
+        
+        self.button = QPushButton('&Load Data')
+        self.button.clicked.connect( self.loadExcelData)
+        layout.addWidget(self.button)
+
+    def loadExcelData(self):
+  
+        df = pd.read_csv('Attendance.csv')
+        if df.size == 0:
+            return
+
+        df.fillna('', inplace=True)
+        self.table.setRowCount(df.shape[0])
+        self.table.setColumnCount(df.shape[1])
+        self.table.setHorizontalHeaderLabels(df.columns)
+
+        # returns pandas array object
+        for row in df.iterrows():
+            values = row[1]
+            for col_index, value in enumerate(values):
+                if isinstance(value, (float, int)):
+                    value = '{0:0,.0f}'.format(value)
+                tableItem = QTableWidgetItem(str(value))
+                self.table.setItem(row[0], col_index, tableItem)
+
+        self.table.setColumnWidth(2, 300)
+        
+if __name__ == '__main__':
+    # don't auto scale when drag app to a different monitor.
+    # QGuiApplication.setHighDpiScaleFactorRoundingPolicy(Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
+    
+   
+  
+
+    app = QApplication(sys.argv)
+    app.setStyleSheet('''
+        QWidget {
+            font-size: 17px;
+        }
+    ''')
+    
+    myApp = MyApp()
+    myApp.show()
+
+    try:
+        sys.exit(app.exec())
+    except SystemExit:
+        print('Closing Window...')
